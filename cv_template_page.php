364c821ae8ff371bff2b85ed2d95af4cdb9d60e2
Abby29 <61682562+Abby29@users.noreply.github.com>
2020-04-15 18:40:51 +0800

    Update cv_template_page.php

--- cv_template_page.php
+++ cv_template_page.php
@@ -9,23 +9,16 @@ Template Name: CV
         <div class="row">
             <section class="col-sm-7 maintext">				
     <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-<div class="post single-page">
+			<div class="post single-page">
 		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
-
-		<div class="postcontent">
+<div class="postcontent">
 			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
 			<?php the_content(); ?>
 		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
         </div><!--postcontet-->
-		
-	</div><!--post class-->
-
-   
+		</div><!--post class-->
 <?php endwhile; ?>
-	
 	<?php else: ?>
 	<div class="post">
 		<p>Sorry, no posts found.</p>
@@ -33,8 +26,7 @@ Template Name: CV
 	<?php endif; ?>
 	</article><!---->
 </section>
-           
-     </div>
+</div>
     </div>
   </div>
 <?php get_footer(); ?>
